512397bbc375a32d27975dce1c2327a5a7042d84
Hong Kim <honki91@gmail.com>
2014-10-12 15:34:26 -0700

    bugfix and email settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "AmyGutmann@freeatnoon.com"
-  def send_time_input_reminder(user)
-    @user = user
-    mail(to: @user.email, subject: "F@N - following up about times!")
-  end
+  default from: "AmyGutmann@freeforcoffee.com"
+
+  # custom emails can be sent using the mailer as below.
+  # def send_time_input_reminder(user)
+  #   @user = user
+  #   mail(to: @user.email, subject: "F@N - following up about times!")
+  # end
 end
