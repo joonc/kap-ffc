1fc6c4b551e374cfc883c876fdbb04b1145b2fe1
Hunty Dumpty <hunter.horsley@gmail.com>
2014-10-12 17:50:07 -0700

    added a method for sending an sms if need to deactivate as well as copy update for smss

--- app/models/twilio_client.rb
+++ app/models/twilio_client.rb
@@ -19,35 +19,42 @@ class TwilioClient
     # promo_str = "Btw, if you know anyone else who’d dig Free At Noon, get them in the group so we can make more matches!"
     promo_str = ""
     potential_msgs =
-      ["Hell yea! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "ERMAHGERD YESSSS! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "Yes? Yes! Woohoo! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "We were hoping you’d say yes! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "Booyah! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "Wahoo!! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "YAYYYYY. Great news. As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "Fantastic! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot.",
-       "Great, thanks! As soon as we know if someone else is free at the same time, we'll put you in a groupme. Love, The FreeAtNoon Bot."]
+      [ "Hell yea! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.”, 
+        "ERMAHGERD YESSSS! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "Yes? Yes! Woohoo! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "We were hoping you’d say yes! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "Booyah! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "Wahoo!! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "YAYYYYY. Great news. Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "Fantastic! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun.",
+        "Great, thanks! Thursday evening the system will put you in a GroupMe with another member who’s also free this week so you can pick a place to meet. Should be fun."]
     msg = "%s %s" % [potential_msgs[rand(8)], promo_str]
     send_sms(to, msg)
   end
 
   def send_wednesday_sms(user)
-    msg = "Hey #{user.first_name}! Are you free today to grab coffee/lunch with another member at the time you put down? Love, The FreeAtNoon Bot.
+    msg = "Hey #{user.first_name}! Are you free this Friday at some point between 1:00 and 3:00 to grab coffee/tea with another member of the FreeForCoffee group in the DP? 
 
-    (Pls text back “yes” or “no”.)"
+(Pls text back “yes” or “no”.)"
     send_sms(user.phone_number,msg)
     user.response = nil
     user.save
   end
 
   def send_welcome_sms(user)
-    msg = "Hey #{user.first_name}! I'm the FreeAtNoon bot; you can just call me F@N. Here’s what’s next: on the days you said you’re free, you’ll get a text from me in the morning. It will ask you if you’re still free for that day. You simply text back a “yes” or a “no.” If you send a “yes” and someone else is free at the same time, you’ll be put into a groupme with them. Then all you have to do is pick the place to meet. That’s it! Glad to have you as part of the FreeAtNoon group! Last thing, it's important you see this: http://bit.ly/SeriousImage."
+    msg = "Hey #{user.first_name}! Welcome to FreeForCoffee (FFC) group within the DP. Here’s what’s next: each Wednesday evening we’ll shoot you a text asking if you’re still free for coffee with another member that Friday between 1:00-3:00. You simply text back a “yes” or a “no.” If you send a “yes”, on Thursday evening you’ll be introduced to another member through GroupMe. Then all you have to do is pick the place to meet sometime between 1-3 Friday. If you say “no” we’ll leave you alone and shoot you another text next Wednesday. If you say “no” three weeks in a row the system will pause your membership so we aren’t spamming you and because the group only works if people make time for it. 
+
+That’s all for now. Glad to have you as part of the FreeForCoffee group in the DP! Hope you enjoy meeting people."
     send_sms(user.phone_number, msg, false)
   end
 
-  def send_policy(user)
-    msg = "Hi #{user.first_name}, me again. Today you said you were free. We weren't able to match you though. Just wanted to explain how matching works -- so you know what to expect in the future. At 10AM, noon, and 2PM a script runs to see if 2 or more people both responded \"yes\" and have windows that overlap. If there is a match, the two people are put in a groupme. If not, we won't message you (to minimize spam). The more people using F@N, the more often there should be matches -- feel free to invite any friends you'd like to have in the group. Hope we can match you next time! Love, The FreeAtNoon Bot."
+  def send_policy_because_couldnt_match(user)
+    msg = "Hi #{user.first_name}! Bad news. We can’t match you for coffee this week. An odd number of people were free and the system pairs groups of 2 so one person couldn’t be paired. It has nothing to do with you specifically. Regardless, we’re sorry to let you down this time. Hope you’ll say “yes” again next week."
+    send_sms(user.phone_number,msg)
+  end
+
+  def send_deactivate_because_no_three_times_sms(user)
+    msg = "Hi #{user.first_name}! It looks like you’ve said “no” to meeting another member of the FreeForCoffee group in the DP the past three weeks. No problem, you’re busy. We’re pausing your membership so you’ll stop receiving invitations. If you have time to particiapte in the future and would like to re-activate your membership shoot us an email: freeforcoffeeteam@gmail.com"
     send_sms(user.phone_number,msg)
   end
 end
