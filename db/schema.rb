cf9807f3104140851c42097f1819eeb7ae1b4502
Hong Kim <honki91@gmail.com>
2014-11-01 12:30:48 -0400

    added major to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019233314) do
+ActiveRecord::Schema.define(version: 20141101161444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20141019233314) do
     t.integer  "year"
     t.string   "team"
     t.string   "fun_fact"
+    t.string   "major"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
